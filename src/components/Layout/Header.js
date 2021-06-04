ca1b773af876e9873f41633cbc9c7214be1e38e2
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2021-06-04 14:32:16 -0400

    Add cart reducer

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,19 +1,31 @@
 import classes from './Header.module.css'
-import {FaShoppingCart} from 'react-icons/fa'
+import { FaShoppingCart } from 'react-icons/fa'
+import CartContext from '../../store/cart-context'
+import { useContext } from 'react'
 
-const Header = ({toggleCart}) => {
+const Header = ({ toggleCart }) => {
+  //Store the result of useContext for later use
+  const cartContext = useContext(CartContext)
+
+  //Use reduce to count the total number of items in the cart
+  const numberOfCartItems = cartContext.items.reduce((curNumber, item) => {
+    return curNumber + item.amount
+  }, 0)
+
   return (
     <header className={classes.header}>
-    <div className={classes.left}>
-    <span className={classes.restaurant}>Green Bite</span>
-    </div>
-    <div className={classes.right}>
-    <div className={classes.button} onClick={toggleCart}>
-    <FaShoppingCart className={classes.cart}/>
-    <span className={classes.cart}>Cart</span>
-    <span id={classes.total} className={classes.cart}>0</span>
-    </div>
-    </div>
+
+      <div className={classes.left}>
+        <span className={classes.restaurant}>Green Bite</span>
+      </div>
+
+      <div className={classes.right}>
+        <div className={classes.button} onClick={toggleCart}>
+          <FaShoppingCart className={classes.cart} />
+          <span className={classes.cart}>Cart</span>
+          <span id={classes.total} className={classes.cart}>{numberOfCartItems}</span>
+        </div>
+      </div>
 
     </header>
   )
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,20 +1,56 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
-const CartProvider = (props) => {
-  const addItemToCart = (item) => { }
-  const removeItemFromCart = (id) => { }
+//Default state for the reducer
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+}
 
-//Recreate the cart context object with pointers to the functions
+//Reducer function which receives state and action and returns new state
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+    //Create a new items array with the new added item
+    const updatedItems = state.items.concat(action.item)
+    //Update the total amount by adding the item price
+    const updatedTotalAmount = state.totalAmount + action.item.amount * action.item.price
+    
+    //Return a new state object with the updated items
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+
+  }
+  return defaultCartState
+}
+
+const CartProvider = (props) => {
+  /*useReducer takes the reducer function and default state and gives us
+  a state snapshot and a function to dispatch actions*/
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
+
+  const addItemToCart = (item) => {
+    //When an item gets added trigger the reducer and pass the item as payload
+    dispatchCartAction({ type: 'ADD', item })
+  }
+
+  const removeItemFromCart = (id) => {
+    dispatchCartAction({ type: 'REMOVE', id })
+  }
+
+  //Recreate the cart context object with pointers to the functions
   const cartContext = {
-    items: [],
-    total: 0,
+    //Grab the values from our state object
+    items: cartState.items,
+    total: cartState.totalAmount,
     addItem: addItemToCart,
     removeItem: removeItemFromCart
   }
 
   return (
     <CartContext.Provider value={cartContext}>
-    {props.children}
+      {props.children}
     </CartContext.Provider>
   )
 }
