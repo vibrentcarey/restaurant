1582197605120d4afb7b048b569f23b87a8b4b12
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2021-06-05 13:15:05 -0400

    Display cart items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,17 @@
+import { useContext } from 'react'
+import CartContext from '../../store/cart-context'
 import classes from './Cart.module.css'
 import Modal from './Modal'
 
 const Cart = ({ toggleCart }) => {
+const cartContext = useContext(CartContext)
+//Extract the name from the items array
+const cartItems = cartContext.items.map(item => {
+  return <div className={classes.item}><h2>{item.name}</h2></div>
+})
+
+const totalAmount = cartContext.total
+
   return (
     <Modal>
       <div className={classes.cart}>
@@ -9,14 +19,15 @@ const Cart = ({ toggleCart }) => {
         <div className={classes.header}>
           <h1>Your Cart</h1>
         </div>
+        <hr/>
 
         <div className={classes.body}>
-          <p>Items</p>
+         {cartItems}
         </div>
 
         <div className={classes.footer}>
           <div className={classes.total}>
-            <h1>Total:</h1>
+            <h1>Total: ${totalAmount}</h1>
           </div>
           <div className={classes.buttons}>
             <button onClick={toggleCart} id={classes.close} className={classes.button}>Close</button>
