3cc1477cae60cf935737da78d762d4f8562053e5
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2021-11-14 11:00:56 -0800

    Add form validation and loading state to cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,14 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import CartContext from '../../store/cart-context'
+import ClipLoader from "react-spinners/ClipLoader";
 import './Cart.css'
 import Modal from './Modal'
 
 
 const Cart = ({ toggleCart }) => {
+  const [orderLoading, setorderLoading] = useState(false);
+  const [orderComplete, setOrderComplete] = useState(false);
+
   const cartContext = useContext(CartContext)
 
   const addItem = (item) => {
@@ -13,6 +17,18 @@ const Cart = ({ toggleCart }) => {
 
   const removeItem = (id) => {
     cartContext.removeItem(id)
+  }
+
+  const handleOrderSubmit = () => {
+    setorderLoading(true)
+    setTimeout(() => {
+      setorderLoading(false)
+      setOrderComplete(true);
+      setTimeout(() => {
+        setOrderComplete(false)
+        toggleCart()
+      }, 3000);
+    }, 3000);
   }
 
   // Cart Items
@@ -32,19 +48,22 @@ const Cart = ({ toggleCart }) => {
 
   const totalAmount = cartContext.total
 
+
   return (
     <Modal>
       <div className="cart">
-
+        {/* Top Of Cart */}
         <div className="cart__header">
           <h1>Your Cart</h1>
         </div>
         <hr className="cart__line" />
-
+        {/* Cart Items */}
         <div className="cart__main">
           {cartItems}
         </div>
-
+        {orderLoading && <ClipLoader color="#e80b40" />}
+        {orderComplete && <h3>Your order has been processed!</h3>}
+        {/* Total And Buttons`` */}
         <div className="cart__footer">
           <div className="cart__total">
             <h1>Total: ${totalAmount.toFixed(2)}</h1>
@@ -52,7 +71,7 @@ const Cart = ({ toggleCart }) => {
           <div className="cart__buttons">
             <button onClick={toggleCart} className="cart__button cart__button--close">Close</button>
             {/* TODO: ADD FEEDBACK AND CLOSE FORM AFTER ORDER */}
-            <button className="cart__button">Order</button>
+            <button className="cart__button" onClick={handleOrderSubmit}>Order</button>
           </div>
         </div>
 
--- src/components/Meals/MenuForm.js
+++ src/components/Meals/MenuForm.js
@@ -1,7 +1,9 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import "./MenuForm.css"
 
 const MenuForm = ({ addToCart }) => {
+  const [inputIsValid, setinputIsValid] = useState(true);
+
   //useRef to reference our input value since we only need it on submit
   const amountInputRef = useRef()
 
@@ -10,17 +12,22 @@ const MenuForm = ({ addToCart }) => {
     //Store the input value in a constant and convert to a number
     const enteredAmount = amountInputRef.current.value
     const enteredAmountNumber = +enteredAmount
-
-    addToCart(enteredAmountNumber)
-
+    if (enteredAmount > 0) {
+      addToCart(enteredAmountNumber);
+    } else {
+      setinputIsValid(false);
+      setTimeout(() => {
+        setinputIsValid(true);
+      }, 3000);
+    }
   }
 
   return (
     <form className="menu_form">
       {/* TODO: ADD FORM VALIDATION */}
       <section className="menu_form__enter">
-      <h3 class="menu_form__title">Amount</h3>
-      <input className="menu_form__input" type='number' pattern="[0-9]*" min='0' max='10' placeholder={1} ref={amountInputRef} />
+        <h3 class="menu_form__title">Amount</h3>
+        <input className={`menu_form__input ${inputIsValid || "menu_form__input--invalid"}`} type='number' pattern="[0-9]*" min='0' max='10' placeholder={0} ref={amountInputRef} />
       </section>
       <br />
       <button onClick={formSubmit} className="menu_form__submit">Add To Cart</button>
